8182642a10145203b689c205f9d04925ca9ec3fc
ahanasil <ahana.sil01@gmail.com>
2025-11-18 14:08:15 -0500

    Added comments
    Added comments to explain functionality.

--- SumUsingThreads.java
+++ SumUsingThreads.java
@@ -3,14 +3,14 @@ public class SumUsingThreads {
 	private static MyThread[] threads;
 
 	static class MyThread extends Thread{
-		private long sum;
+		private long sum; //sum
 
-		public long getSum(){
+		public long getSum(){ //getter for sum
 			return sum;
 		}
 
 		@Override
-		public void run(){
+		public void run(){ //loops from 1 to 1,000,000 and sums
 			long s = 0;
 			for(int i = 1; i <= 1000000; i++){
 				s++;
@@ -20,9 +20,9 @@ public class SumUsingThreads {
 	}
 	public static void main(String[] args){
 
-		long start = System.nanoTime();
+		long start = System.nanoTime(); //start time
 
-		threads = new MyThread[1000];
+		threads = new MyThread[1000]; //creates array of threads
 		for(int i = 0; i < threads.length; i++){
 			threads[i] = new MyThread();
 			threads[i].start();
@@ -30,7 +30,7 @@ public class SumUsingThreads {
 
 		for(MyThread t : threads){
 			try {
-				t.join();
+				t.join(); //join threads to main
 			} catch (InterruptedException e){
 				e.printStackTrace();
 			}
@@ -38,10 +38,10 @@ public class SumUsingThreads {
 
 		long sumT = 0;
 		for(MyThread thread : threads){
-			sumT += thread.getSum();
+			sumT += thread.getSum(); //sums the sums of all threads
 		}
 
-		long end = System.nanoTime();
+		long end = System.nanoTime(); //end time
 
 		System.out.println("Sum: " + sumT);
         System.out.println("Time (ns): " + (end - start));
